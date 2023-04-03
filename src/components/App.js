35a562f8062e0fe7a9f031e1f000afabb6b8c8e1
Mateusz Siciarz <siciarz.mat@gmail.com>
2023-04-03 18:15:10 +0200

    GH pages fix

--- src/components/App.js
+++ src/components/App.js
@@ -1,35 +1,35 @@
-import React, { useState, useEffect } from "react";
-import styled from "styled-components";
-import { StyledHeader } from "./Header";
-import { SearchBar } from "./SearchBar";
-import { fetchImages } from "services/api";
-import { ImageGallery } from "./ImageGallery";
-import { Loader } from "./Loader";
-import { Error } from "./Error";
-import { StyledButton } from "./Button";
+import React, { useState, useEffect } from 'react';
+import styled from 'styled-components';
+import { StyledHeader } from './Header';
+import { SearchBar } from './SearchBar';
+import { fetchImages } from 'services/api';
+import { ImageGallery } from './ImageGallery';
+import { Loader } from './Loader';
+import { Error } from './Error';
+import { StyledButton } from './Button';
 
 const StyledApp = styled.div`
-    display: grid;
-    grid-template-columns: 1fr;
-    grid-gap: 16px;
-    padding-bottom: 24px;
-`
+  display: grid;
+  grid-template-columns: 1fr;
+  grid-gap: 16px;
+  padding-bottom: 24px;
+`;
 export const App = () => {
-  const [query, setQuery] = useState("");
+  const [query, setQuery] = useState('');
   const [page, setPage] = useState(1);
   const [isLoading, setIsLoading] = useState(false);
   const [images, setImages] = useState([]);
   const [error, setError] = useState(null);
   const [lastPage, setLastPage] = useState(1);
 
-  const handleSubmit = (query) => {
+  const handleSubmit = query => {
     setQuery(query);
     setPage(1);
-  }
+  };
 
   useEffect(() => {
     const handleImagesRequest = async () => {
-      setIsLoading(true)
+      setIsLoading(true);
 
       try {
         const fetchedData = await fetchImages(query, page);
@@ -38,32 +38,30 @@ export const App = () => {
         setLastPage(lastPage);
 
         if (page === 1) {
-          setImages(fetchedData.hits)
+          setImages(fetchedData.hits);
         } else {
-          setImages(prevState => ([...prevState, ...fetchedData.hits])
-          );
+          setImages(prevState => [...prevState, ...fetchedData.hits]);
         }
       } catch (error) {
-        setError(error.message)
+        setError(error.message);
       } finally {
         setIsLoading(false);
       }
-    }
+    };
     if (query) {
       handleImagesRequest();
     }
-  }, [query, page])
+  }, [query, page]);
 
   useEffect(() => {
     if (page === 1) {
-      window.scrollTo({ top: 0, behavior: "smooth" });
+      window.scrollTo({ top: 0, behavior: 'smooth' });
     }
   }, [page]);
 
-
   const handleClick = () => {
-    setPage(page + 1)
-  }
+    setPage(page + 1);
+  };
 
   return (
     <StyledApp>
@@ -72,11 +70,12 @@ export const App = () => {
       </StyledHeader>
       <ImageGallery images={images} />
       {isLoading && <Loader />}
-      {page !== lastPage && images.length > 0 && !isLoading
-        ?
+      {page !== lastPage && images.length > 0 && !isLoading ? (
         <StyledButton onClick={handleClick}>Load More</StyledButton>
-        : null}
-      {images.length === 0 && query && !isLoading && <Error text="Nothing found! Try again" />}
+      ) : null}
+      {images.length === 0 && query && !isLoading && (
+        <Error text="Nothing found! Try again" />
+      )}
       {error && <Error text="An error occurred. Please try again" />}
     </StyledApp>
   );
